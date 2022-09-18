796f5ae206fd2c7f1ec52fed7cfe9b2c80f59dd0
Anatoly Finchenko <finchenko777@gmail.com>
2022-09-18 08:44:23 +0300

    *firstA*

---
+++ homework5 1a.py
@@ -0,0 +1,39 @@
+# Играют два игрока делая ход друг после друга.
+import random
+
+
+def player_step():
+    try:
+        while True:
+            num = int(input('Возьми конфеты: '))
+            if 0 < num < 29:
+                break
+            else:
+                print('Мухлюешь')
+        return num
+    except:
+        print('Это не число!')
+
+
+name_1 = input('Введите имя: ')
+name_2 = input('Введите имя: ')
+lot = random.randint(1, 2)
+amount = int(input('Введите количество конфет : '))
+candy = 28
+while True:
+    print(f'Осталось {amount} конфет(ы). Можешь взять от 1 до 28 конфет  ход у игрока {lot}')
+    if lot == 1:
+        step_1 = player_step()
+        amount -= step_1
+        lot = 2
+    else:
+        step_2 = player_step()
+        amount -= step_2
+        print(f'{name_2} взял {step_2}')
+        lot = 1
+    if amount < 1:
+        if lot == 1:
+            print(f'Победил игрок {name_1} ')
+        else:
+            print(f'Победил игрок {name_2} ')
+        break
